15fd7d33f46c70a9592c3fa30f2a910068c73709
Sam Van Battel <teacher.svb@gmail.com>
2020-11-25 20:56:07 +0100

    update

--- assets/js/firebase-io.js
+++ assets/js/firebase-io.js
@@ -1,17 +1,28 @@
-
-
 function saveToFirebase(userid, userdata) {
     firebase.database().ref('recipes').child(JSONrecipe.id).set(JSONrecipe)
-        .then(function(snapshot) {
+        .then(function (snapshot) {
             //success(); // some success method
-        }, function(error) {
+        }, function (error) {
             console.log('error' + error);
             //error(); // some error method
         });
 }
 
-function readUserDataFromFirebase(uid) {
-    return firebase.database().ref(`users/${uid}`).once('value').then(function(snapshot) {
+function readUser(uid) {
+    return firebase.database().ref(`users/${uid}`).once('value').then(function (snapshot) {
+        userdata = snapshot;
+
+        const urlParams = new URLSearchParams(window.location.search);
+        let recipeId = urlParams.get('leerling');
+
+        if (userdata.val().admin) {
+            adminReadUsers();
+        }
+    });
+}
+
+function adminReadUsers() {
+    return firebase.database().ref(`users`).once('value').then(function (snapshot) {
         userdata = snapshot;
 
         console.log(userdata.val());
@@ -19,7 +30,7 @@ function readUserDataFromFirebase(uid) {
         const urlParams = new URLSearchParams(window.location.search);
         let recipeId = urlParams.get('select');
 
-        let valueFound = snapshot.forEach(function(childSnapshot) {
+        let valueFound = snapshot.forEach(function (childSnapshot) {
             user = childSnapshot.val();
             console.log(user);
         });
