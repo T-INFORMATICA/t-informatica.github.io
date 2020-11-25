e6e6284a8f5293d48b052d81f3f34722d0299249
Sam Van Battel <teacher.svb@gmail.com>
2020-11-25 21:49:38 +0100

    Update lms.js

--- assets/js/lms.js
+++ assets/js/lms.js
@@ -3,8 +3,12 @@ function initialize(user) {
 }
 
 function addUserToMenu(user) {
+    if (document.querySelector(`#${user.uid}`)) {
+        return;
+    }
+
     let tmpl = `
-            <h2><a href="{{baseurl}}/profiel.html?leerling=${user.uid}">${user.naam}</a></h2>
+            <h2 id="${user.uid}"><a href="{{baseurl}}/profiel.html?leerling=${user.uid}">${user.naam}</a></h2>
             <a href="{{baseurl}}/rapport.html?leerling=${user.uid}">Rapport</a>
             <a href="{{baseurl}}/evaluaties.html?leerling=${user.uid}">Evaluaties</a>
     `;
