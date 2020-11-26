e41fa8396603cd0f161a4114f9d1ad6175dc3565
Sam Van Battel <teacher.svb@gmail.com>
2020-11-26 12:38:19 +0100

    Update firebase-io.js

--- assets/js/firebase-io.js
+++ assets/js/firebase-io.js
@@ -7,31 +7,3 @@ function saveToFirebase(userid, userdata) {
             //error(); // some error method
         });
 }
-
-
-function readUser(uid) {
-    return firebase.database().ref(`users/${uid}`).once('value').then(function (snapshot) {
-
-        const urlParams = new URLSearchParams(window.location.search);
-        let recipeId = urlParams.get('leerling');
-
-        userdata = snapshot;
-        addUserToMenu(userdata.key, userdata.val());
-        if (userdata.val().admin) {
-            adminReadUsers();
-        }
-    });
-}
-
-function adminReadUsers() {
-    return firebase.database().ref(`users`).once('value').then(function (snapshot) {
-        userdata = snapshot;
-
-        let valueFound = snapshot.forEach(function (childSnapshot) {
-            userdata = childSnapshot;
-            addUserToMenu(userdata.key, userdata.val());
-        });
-
-        addAdmincontrolsToMenu();
-    });
-}
