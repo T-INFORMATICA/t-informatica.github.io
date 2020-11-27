dad3920130822a0dba779bf1fba7cf9ab24db292
Sam Van Battel <teacher.svb@gmail.com>
2020-11-27 10:22:59 +0100

    update

--- assets/js/firebase-auth-ui.js
+++ assets/js/firebase-auth-ui.js
@@ -39,3 +39,11 @@ function authUser(user) {
         ui.start('#firebaseui-auth-container', uiConfig);
     }
 }
+
+function signOut() {
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
+}
