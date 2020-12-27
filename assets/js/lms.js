2c56b541d2799d4ab20b300c0ae9677d78f89b23
Sam Van Battel <teacher.svb@gmail.com>
2020-12-27 20:54:04 +0100

    Update lms.js

--- assets/js/lms.js
+++ assets/js/lms.js
@@ -1,3 +1,49 @@
+function toCssSafeId(text) {
+    text = text.replace(/[!\s\"#$%&'\(\)\*\+,\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '');
+    return text;
+}
+
+function calculateResults(resultsArr) {
+    let evalProgress = {
+        "A": {
+            "A": 0, "B": -0.5, "C": -1.5, "D": -1.5, "E": -1.5
+        },
+        "B": {
+            "A": 1, "B": 0, "C": -1, "D": -1.5, "E": -1.5
+        },
+        "C": {
+            "A": 1.5, "B": 0.5, "C": 0, "D": -1, "E": -1.5
+        },
+        "D": {
+            "A": 2, "B": 1, "C": 0.5, "D": 0, "E": -1.5
+        },
+        "E": {
+            "A": 2, "B": 1.5, "C": 1, "D": 0, "E": 0
+        },
+    }
+
+    // convert letter to number
+    let letters = ["E", "D", "C", "B", "A"];
+    let result = resultsArr[0];
+    let resultNumber = letters.indexOf(result) - 2; // -2 to offset the index between -2 and 2, instead of 0 and 5
+
+    for (let i = 1; i < resultsArr.length; ++i) {
+
+        let resultLetter = letters[Math.round(resultNumber + 2)];
+        let behaaldeLetter = resultsArr[i];
+        let resultDelta = evalProgress[resultLetter][behaaldeLetter];
+
+        resultNumber += resultDelta;
+    }
+
+    let resultLetter = letters[Math.round(resultNumber + 2)];
+    return resultLetter;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+
 function addUsersToMenu(userid) {
     let database = firebase.database();
     let userref = database.ref(`users/${userid}`);
@@ -83,145 +129,10 @@ function addUserdataToProfileTable(userid) {
     );
 }
 
-/////////////////////////////
-
-function toCssSafeId(text) {
-    text = text.replace(/[!\s\"#$%&'\(\)\*\+,\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '');
-    return text;
-}
-
-function calculateResults(resultsArr) {
-    let evalProgress = {
-        "A": {
-            "A": 0, "B": -0.5, "C": -1.5, "D": -1.5, "E": -1.5
-        },
-        "B": {
-            "A": 1, "B": 0, "C": -1, "D": -1.5, "E": -1.5
-        },
-        "C": {
-            "A": 1.5, "B": 0.5, "C": 0, "D": -1, "E": -1.5
-        },
-        "D": {
-            "A": 2, "B": 1, "C": 0.5, "D": 0, "E": -1.5
-        },
-        "E": {
-            "A": 2, "B": 1.5, "C": 1, "D": 0, "E": 0
-        },
-    }
-
-    // convert letter to number
-    let letters = ["E", "D", "C", "B", "A"];
-    let result = resultsArr[0];
-    let resultNumber = letters.indexOf(result) - 2; // -2 to offset the index between -2 and 2, instead of 0 and 5
-
-    for (let i = 1; i < resultsArr.length; ++i) {
-
-        let resultLetter = letters[Math.round(resultNumber + 2)];
-        let behaaldeLetter = resultsArr[i];
-        let resultDelta = evalProgress[resultLetter][behaaldeLetter];
-
-        resultNumber += resultDelta;
-    }
-
-    let resultLetter = letters[Math.round(resultNumber + 2)];
-    return resultLetter;
-}
-/*
-function addAdmincontrolsToMenu() {
-    let user = firebase.auth().currentUser;
-    return firebase.database().ref(`users/${user.uid}`).once('value').then(function (snapshot) {
-        userdata = snapshot.val();
-
-        if (userdata.admin === false) {
-            return null;
-        }
-
-        let tmpl = `
-                <hr>
-                <h2>Admin</h2>
-                <a href="./nieuweEval.html">Nieuwe Evaluatie</a>
-                <hr>
-        `;
-        document.querySelector('#leftMenu>#leftMenu-logout').insertAdjacentHTML('afterend', tmpl);
-
-        addManagedUsersToMenu();
-    });
-}
-/*
-function addCurrentUserToMenu() {
-    let user = firebase.auth().currentUser;
-    return firebase.database().ref(`users/${user.uid}`).once('value').then(function (snapshot) {
-        userdata = snapshot.val();
-
-        let tmpl = `
-            <h2>Profiel</h2>
-            <a href="./index.html" id="${user.uid}">${userdata.naam}</a>
-        `;
-        if (!userdata.admin) {
-            tmpl += `
-            <a href="./rapport.html">Rapport</a>
-            <a href="./eval.html">Evaluaties</a>
-            `;
-        }
-
-        document.querySelector('#leftMenu>h1').insertAdjacentHTML('afterend', tmpl);
-    });
-}
-
-function addManagedUsersToMenu() {
-    return firebase.database().ref(`users`).once('value').then(function (snapshot) {
-        let tmpl = `
-            <h2>Manage Users</h2>
-        `;
-        document.querySelector('#leftMenu').innerHTML += tmpl;
-
-        snapshot.forEach(function (childSnapshot) {
-            userdata = childSnapshot;
-            if (userdata.val().admin) {
-                return;
-            }
-            uid = childSnapshot.key;
-
-            let tmpl = `
-                <a href="./profile.html?userid=${uid}">${userdata.val().naam}</a>
-                <a href="./rapport.html?userid=${uid}">Rapport</a>
-                <a href="./eval.html?userid=${uid}">Evaluaties</a>
-            `;
-
-            document.querySelector('#leftMenu').innerHTML += tmpl;
-        });
-    });
-}
-
-
-/*
-function createMenu() {
-    addCurrentUserToMenu();
-    addAdmincontrolsToMenu();
-}
-
-function createProfile(userId) {
-    return firebase.database().ref(`users/${userId}`).once('value').then(function (snapshot) {
-        snapshot.forEach(function (childSnapshot) {
-            userdata = childSnapshot;
-            switch (userdata.key) {
-                case "username":
-                    document.querySelector('#profileUsername').innerHTML += userdata.val();
-                    break;
-                case "naam":
-                    document.querySelector('#profileName').innerHTML += userdata.val();
-                    break;
-                case "password":
-                    document.querySelector('#profilePassword').innerHTML += userdata.val();
-                    break;
-                case "url":
-                    document.querySelector('#profileUrl').innerHTML += userdata.val();
-                    break;
-            }
-        });
-
-    });
-}*/
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+//////// TODO - REWORK FUNCTION
 
 function createResults(userId) {
     let db = firebase.database().ref();
