c338ae2c6705933bd9e9244272f179c1b95afda8
Sam Van Battel <teacher.svb@gmail.com>
2021-05-09 21:21:57 +0200

    Update exercise.js

--- assets/js/exercise.js
+++ assets/js/exercise.js
@@ -55,7 +55,7 @@ function EvaluateExercise() {
             console.log(JSON.stringify(questions));
 
             for (const [term, currentResult] of Object.entries(terms)) {
-                let termCount = entries.reduce((i, it) => it[1].term === term ? ++i : i, 0);
+                let termCount = questions.reduce((i, it) => it[1].term === term ? ++i : i, 0);
                 console.log(termCount);
                 console.log(term);
                 userref
