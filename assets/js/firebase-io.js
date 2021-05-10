4662db6aeae384ccaff45f2fdeb14f03cfc1023b
teacher-svb <39157063+teacher-svb@users.noreply.github.com>
2021-05-10 08:40:11 +0200

    removed unused file

--- assets/js/firebase-io.js
+++
@@ -1,9 +0,0 @@
-function saveToFirebase(userid, userdata) {
-    firebase.database().ref('recipes').child(JSONrecipe.id).set(JSONrecipe)
-        .then(function (snapshot) {
-            //success(); // some success method
-        }, function (error) {
-            console.log('error' + error);
-            //error(); // some error method
-        });
-}
