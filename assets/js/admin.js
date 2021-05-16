5ea0b02ba592d2e7c7e7e0f7793f844383b630ad
Sam Van Battel <teacher.svb@gmail.com>
2021-05-16 18:11:51 +0200

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -8,6 +8,7 @@ function createNewEvalForm() {
 function evalChangeStudent(offset) {
     let currentIndex = parseInt(document.querySelector("#evalStudentSelection").selectedIndex, 10);
     document.querySelector("#evalStudentSelection").selectedIndex = currentIndex + offset;
+    evalChangeStudent();
 }
 
 function evalChangeStudent() {
