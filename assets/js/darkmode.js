32ca1b83120f9fbdeb310df7cac8d053fd77c903
teacher-svb <39157063+teacher-svb@users.noreply.github.com>
2021-06-09 09:16:25 +0200

    Update darkmode.js

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,9 +1,21 @@
+documentReady(function () {
+    var darkmode = localStorage.getItem('darkmode');
+    //var lang = getCookie("lang");
+    // console.log(lang);
+    if (darkmode == "light") {
+        ChangeDarkMode();
+    }
+});
+
 function ChangeDarkMode() {
     
     if (document.querySelector("body").classList.contains("light-theme")) {
         document.querySelector("body").classList.remove("light-theme");
+        localStorage.setItem('darkmode', 'dark');
+
     }
     else {
         document.querySelector("body").classList.add("light-theme");
+        localStorage.setItem('darkmode', 'light');
     }
 }
